1fd6aa717f56dcf100ada5ec0d9d7619f4758b2f
RiyyaduSolihin <riyyadu1234@gmail.com>
2026-01-06 16:39:36 +0700

    Mengerjakan Koneksi DB yang baru

--- Main.java
+++ Main.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ManajemenBarberShop;
 
-/**
- *
- * @author riyya
- */
+import ManajemenBarberShop.model.PelangganModel;
+import ManajemenBarberShop.view.MainView;
+import ManajemenBarberShop.controller.PelangganController;
+import javax.swing.SwingUtilities;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Menjalankan di Event Dispatch Thread agar Thread-Safe
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Inisialisasi MVC
+                PelangganModel model = new PelangganModel();
+                MainView view = new MainView();
+                
+                // Menghubungkan View dan Model lewat Controller
+                new PelangganController(model, view);
+                
+                // Tampilkan Aplikasi
+                view.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
